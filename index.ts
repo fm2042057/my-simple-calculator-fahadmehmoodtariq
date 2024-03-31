0d435f530e8f5cf85f7ceb798c957bf0755062aa
Fahad Mehmood <159702945+fm2042057@users.noreply.github.com>
2024-03-31 07:57:18 +0500

    some change in line 8 and 9

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
       message: "Select one of the operator to perform operation",
       type: "list",
